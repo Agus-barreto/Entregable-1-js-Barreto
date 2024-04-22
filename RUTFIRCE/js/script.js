575d6a6a1b6d24bd928fef218bf05f05313838cc
Agus Barreto <agustinabarreto638@gmail.com>
2024-04-22 14:28:19 -0300

    reentrega

--- RUTFIRCE/js/script.js
+++ RUTFIRCE/js/script.js
@@ -19,80 +19,30 @@ ingreso=true
 alert("Esperamos que disfrutes la recorrida por nuestro sitio web, estamos a las ordenes!")
 
 //----------------------------------------------------------
-//variable
-let prendajeans1 = "Wide Leg Recto";
-let prendajeans2 = "Jeans Chupin";
-let prendajeans3 = "Jeans Wide Leg";
 
-const nombredetienda = "RutFirce"
+const prendajeans1 = "Jeans Wide Leg Recto";
+const prendajeans2 = "Jeans Chupin";
+const prendajeans3 = "Jeans Wide Leg";
 
 //---------------------------------------------------------
+
 //arrays
-const producto1 ={
-    nombre:"Jeans Wide Leg Recto",
-    codigo: V15,
-    precio:1190,
-}
-console.log(producto1.nombre) ;'Jeans Wide Leg Recto'
+const productos = [prendajeans1,prendajeans2]
+console.log(productos)
 
-const producto2 = {
-    nombre: "Jeans chupin", 
-    codigo: v16,
-    precio: 1090,
-}
-console.log(producto2.nombre);  'Jeans Chupin'
-
-const producto3 ={
-    nombre: "Jeans Wide Leg", 
-    codigo: v17,
-    precio: 990,
-}
-console.log(producto3.nombre) ; 'Jeans Wide Leg'
-
-const productos = ['Jeans Wide Leg Recto','Jeans Chupin', 'Jeans Wide Leg']
+productos.push(prendajeans3)
+console.log(productos)
 
 for (let i = 0; i < productos.length; i++){
     console.log(productos[i])
 }
-
-
-
 //----------------------------------------------------------
-//ciclos
-for(let i=0;i<9;i++){
-    console.log('Interación Nº ' + i);
-}
+ 
+//funcion 
 
-//cantidad de jeans wide leg recto
+const IVA = 0.22 
+const IVAenprendas = precio => precio*IVA
 
-for(let i=1;i<=6;i++){
-    if(i==5){
-        console.warn("Interracion interrumpida en el ciclo", i , "por la capsula BREAK.")
-        break
-    }
-    alert(i);
-}
+const jeanschupin = IVAenprendas(1200)
 
-//----------------------------------------------------------
-
-//suma de pedidos
-function sumar(){
-    const primeraprenda = Number(prompt(1190))
-    const segundaprenda = Number(prompt(1090))
-return 1190 + 1090
-}
-
-let resutado =2280
-
-function sumar (prendajeans1,prendajeans2){
-    resultado= prendajeans1+prendajeans2;
-}
-        
-function mostrar(mensaje){
-    console.log(mensaje);
-}
-
-console.log (sumar)
-console.log (sumar())
-
-
+console.log(jeanschupin)
---
+++ RUTFIRCE/js/sumar.js
@@ -0,0 +1,6 @@
+//----------------------------------------------------------
+//---------------------------------------------------------
+function sumar() {
+    const primeraprenda = prenda(prompt("Ingrese la primera  prenda que desee"));
+    const segundaprenda = prenda(prompt("Ingrese la  segunda prenda que desee"));
+}
